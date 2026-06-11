339530858da76a1660822e7545cc8c5bb5aec9b7
DebboyRohan <rohankumarpandey@kgpian.iitkgp.ac.in>
2026-06-11 11:59:11 +0530

    Updated nextconfig for development enviroment

--- next.config.ts
+++ next.config.ts
@@ -14,8 +14,21 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "pub-4cc352379fe147fe9e7be4022d104523.r2.dev"
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+      },
+      {
+        protocol: "https",
+        hostname: "*.crackncet.com",
       }
     ],
+    dangerouslyAllowLocalIP: true,
   },
 };
 
